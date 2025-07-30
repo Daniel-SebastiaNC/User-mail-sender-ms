b4bcb22f0f611862589b9dabe7c2d958d8452407
Daniel-SebastiaNC <daniel.sebastian.nc@gmail.com>
2025-07-29 22:17:11 -0300

    feat: add UserModel and your DTO and Repository

---
+++ user/src/main/java/dev/java10x/user/model/UserModel.java
@@ -0,0 +1,23 @@
+package dev.java10x.user.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "tb_user")
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class UserModel {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+}
